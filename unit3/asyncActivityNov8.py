adda859379cbd58c1ed89d234ea6ccce6ea5ef8a
mike <youngbulmike@gmail.com>
2024-11-12 13:46:59 +0000

    async

---
+++ unit3/asyncActivityNov8.py
@@ -0,0 +1,41 @@
+"a string concatenation is combining strings together"
+#An example is 
+story = " I will be going to"
+location='the dinner'
+time ='at 6:30'
+print(story,location, time)
+
+
+'we write variables by first delclaring/ giving it a name, then we use the equal sigh, then give it data'
+
+'1. you cannot have a number or symbol start a variable name'
+
+'2.  Variable should ideally be short and decriptive'
+
+'3. we can format our variables namws using '
+# camel case = when naming variable 
+# every word afetr the first word is capitalized 
+loginName=''
+resetPasswordcode=''
+
+# Snake Case = when naming a variable, every word after
+# the first word is spaced out with an underscore.
+login_name=''
+reset_password_code=''
+
+# Pascal case = when naminga a variable, every word is capitalized
+LoginName=''
+ResetPasswordCode=''
+
+a=3
+b=100
+
+print(a*b)
+
+x = 10
+
+y= 2
+
+z= y*2
+
+print(x > 1 and z=4)
