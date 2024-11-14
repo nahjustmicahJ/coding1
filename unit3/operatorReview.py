f30ec5198cd01b932e972463c934ac144b003dc0
mike <youngbulmike@gmail.com>
2024-11-14 13:20:01 +0000

    updated bc it didnt save

--- unit3/operatorReview.py
+++ unit3/operatorReview.py
@@ -5,3 +5,28 @@
 # logical
 
 # comparison
+# assisgnemt operators- doing arithmetic after the assignmentof a value.
+# assignment is primarily focused on the yhe 
+
+# arithmetic - math process, addition, subtraction, multiplication, an
+
+depositAmount= 100.0
+
+withdrawAmunt= 100.0
+
+checkingAccountBalance=0.0
+
+savingsAccountBalance= 300.0
+investmentAccount= 0.0
+
+print(depositAmount+ savingsAccountAlance)
+
+# track runner app arithmetic operator examples
+firstRunTime=20.00
+secoundRunTime= 18.00
+runningDifference= firstRunTime- secoundRunTime
+
+
+# logical
+
+# comparison
