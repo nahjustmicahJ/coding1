843459aff430ef6b9d0f995a51bd8e27cbf3cbdd
mike <youngbulmike@gmail.com>
2024-11-19 13:47:37 +0000

    ran the python

---
+++ unit3/functions.py
@@ -0,0 +1,29 @@
+# A function is a set of instructions that will perform a 
+# specific task
+
+# there are two variations of functions: user-defined functions
+#and built-in functions.
+
+# built-in functions: instructions that have already been written,
+# are built-in to the programming language
+
+print('coding class') # print is a built-in function. we can identify functions 
+# by the round brackets.
+
+name= input('what is your name?')
+
+print(name)
+print(3*number)
+
+# all date types, variables and operators are just places for us
+# to use order to write instructions for functions
+
+# user defined functions - custom functions created by an engineer.
+
+# how do you write a user defined function.
+
+def pbAndj():
+    print(' step 1. grab two slices of bread')
+    print(' step 2. spread peanut butter on one side of bread')
+    print(' step 3. spread jelly on the other side of the bread')
+    print(' step 4. put the sandwich together with spreads')
