6773a890172749c8181f34d12b46063d5152ce4e
mike <youngbulmike@gmail.com>
2024-12-19 20:37:53 +0000

    completed

---
+++ unit3/holidayExtraCredit.py
@@ -0,0 +1,16 @@
+def holidayPlanner(date):
+    if date== "12/21/2024":
+        print('I am going to chill')
+    elif date== "12/31/2024":
+        print('I am going to a party') 
+    elif date== "1/1/2025":
+        print('I am going to another party')
+    elif date== "1/3/2025":
+        print('I am going to chill')
+    elif date== "1/7/2025":
+        print('I am going back to school')
+    else:
+        print("Sorry there is nothing planned")
+    
+
+holidayPlanner("12/22/2024")
