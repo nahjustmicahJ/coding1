84e4612441285ba721f98de49bc37081be4f0c1a
mike <youngbulmike@gmail.com>
2025-01-09 13:32:07 +0000

    did the old async

---
+++ unit3/asyncActivity_nov1.py
@@ -0,0 +1,10 @@
+a=200
+b=45
+
+print(a+b)
+
+"a list would be the best to store all the data"
+"i would use the integer data type for numeric datas of the week"
+"i would use string data type for the word/string days of the week."
+"An integer data type stores whole numbers without decimal points, while a float data type stores numbers with decimal places"
+"I don't quite understand the question"
