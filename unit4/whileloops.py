317151ff7a3178f8f51e791fa91498a059bb43e6
mike <youngbulmike@gmail.com>
2025-01-22 13:44:10 +0000

    followed along

---
+++ unit4/whileloops.py
@@ -0,0 +1,11 @@
+# loops - is a construct that repeats a set of instructions
+# under specific conditions.
+
+# while loop - so long as a condition is true, 
+# keep repeating the loop
+
+x = 2
+y = 'mike'
+#while x == 2:
+while 10 > 1:
+    print("The input will run forever")
